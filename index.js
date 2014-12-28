a45b777e484aaa7660fe69c9f4bc6e0ebfea8e71
Andrey Sitnik <andrey@sitnik.ru>
2014-12-29 00:39:29 +0100

    Use prereleased PostCSS 4.0

--- index.js
+++ index.js
@@ -6,65 +6,40 @@ var selector = function (parent, node) {
     }
 };
 
-var move = function (what, where, after) {
-    var clone = what.clone();
-    what.removeSelf();
-    where.insertAfter(after, clone);
-    return clone;
-};
-
 var atruleChilds = function (rule, atrule) {
     var clone;
     var decls = [];
-    atrule.each(function (node) {
-        if ( node.type == 'decl' ) {
-            clone = node.clone();
-            clone.before = node.before;
-            decls.push( clone );
-            node.removeSelf();
-        } else if ( node.type == 'rule' ) {
-            node.selector = selector(rule, node);
-        } else if ( node.type == 'atrule' ) {
-            atruleChilds(rule, node);
+    atrule.each(function (child) {
+        if ( child.type == 'decl' ) {
+            decls.push( child );
+        } else if ( child.type == 'rule' ) {
+            child.selector = selector(rule, child);
+        } else if ( child.type == 'atrule' ) {
+            atruleChilds(rule, child);
         }
     });
     if ( decls.length ) {
-        clone = rule.clone({ childs: [] });
-        for ( var i = 0; i < decls.length; i++ ) {
-            clone.append( decls[i] );
-        }
+        clone = rule.clone({ nodes: [] });
+        for ( var i = 0; i < decls.length; i++ ) decls[i].moveTo(clone);
         atrule.prepend(clone);
-        clone.before = atrule.before;
     }
 };
 
-var moveAtrule = function (rule, atrule, after) {
-    atruleChilds(rule, atrule);
-    return move(atrule, rule.parent, after || rule.parent);
-};
-
-var moveRule = function (rule, after) {
-    var to     = rule.parent.parent;
-    var before = rule.before;
-    var moved  = move(rule, to, after || rule.parent);
-    moved.selector = selector(rule.parent, moved);
-    moved.before   = rule.before;
-    return moved;
-};
-
 var rule = function (rule) {
-    var unwrapped   = false;
-    var insertAfter = false;
+    var unwrapped = false;
+    var after     = rule;
     rule.each(function (child) {
         if ( child.type == 'rule' ) {
             unwrapped = true;
-            insertAfter = moveRule(child, insertAfter);
+            child.selector = selector(rule, child);
+            after = child.moveAfter(after);
         } else if ( child.type == 'atrule' ) {
             unwrapped = true;
-            insertAfter = moveAtrule(rule, child, insertAfter);
+            atruleChilds(rule, child);
+            after = child.moveAfter(after);
         }
     });
-    if ( unwrapped && rule.childs.length === 0 ) rule.removeSelf();
+    if ( unwrapped && rule.nodes.length === 0 ) rule.removeSelf();
 };
 
 var process = function (node) {
--- test/test.js
+++ test/test.js
@@ -11,13 +11,13 @@ var check = function (input, output) {
 describe('postcss-nested', function () {
 
     it('unwraps rule inside rule', function () {
-        check('a { a: 1; b { b: 2; c { c: 3 } } }',
-              'a { a: 1 } a b { b: 2 } a b c { c: 3 }');
+        check('a { a: 1 } a { a: 1; b { b: 2; c { c: 3 } } }',
+              'a { a: 1 } a { a: 1 } a b { b: 2 } a b c { c: 3 }');
     });
 
     it('cleans rules after unwrap', function () {
         check('a { b .one {} b .two {} }',
-              'a b .one {} a b .two {}');
+              'a b .one {}\na b .two {}');
     });
 
     it('replaces ampersand', function () {
@@ -26,33 +26,65 @@ describe('postcss-nested', function () {
     });
 
     it('replaces ampersands', function () {
-        check('a { &:hover, &:active { } }',
-              'a:hover, a:active { }');
+        check('a { &:hover, &:active {} }',
+              'a:hover, a:active {}');
     });
 
     it('replaces ampersand in string', function () {
-        check('.block { &_elem { } }',
-              '.block_elem { }');
+        check('.block { &_elem {} }',
+              '.block_elem {}');
     });
 
     it('unwrap rules inside at-rules', function () {
-        check('@media (max-width: 500px) { a { b { } } }',
-              '@media (max-width: 500px) { a b { } }');
+        check('@media (max-width: 500px) { a { b {} } }',
+              '@media (max-width: 500px) { a b {} }');
     });
 
     it('unwraps at-rule', function () {
-        check('.block { .el { @media (max-width: 500px) { width: auto } } }',
-              '@media (max-width: 500px) { .block .el { width: auto } }');
+        check('a { b { @media screen { width: auto } } }',
+              '@media screen {\n    a b {\n        width: auto\n    }\n}');
     });
 
     it('unwraps at-rule with rules', function () {
-        check('a { @media (max-width: 500px) { b { color: black } } }',
-              '@media (max-width: 500px) { a b { color: black } }');
+        check('a { @media screen { b { color: black } } }',
+              '@media screen {\n    a b {\n        color: black\n    }\n}');
     });
 
     it('unwraps at-rules', function () {
-        check('a { @media (max-width: 5px) { @supports (a: 1) { a: 1 } } }',
-              '@media (max-width: 5px) { @supports (a: 1) { a { a: 1 } } }');
+        check('a { a: 1 } a { @media screen { @supports (a: 1) { a: 1 } } }',
+              'a { a: 1 } @media screen { @supports (a: 1) { a { a: 1 } } }');
+    });
+
+    it('parses example', function () {
+        var input =  '.phone {\n' +
+                     '    &_title {\n' +
+                     '        width: 500px;\n' +
+                     '        @media (max-width: 500px) {\n' +
+                     '            width: auto;\n' +
+                     '        }\n' +
+                     '    }\n' +
+                     '    body.is_dark &_title {\n' +
+                     '        color: white;\n' +
+                     '    }\n' +
+                     '    img {\n' +
+                     '        display: block;\n' +
+                     '    }\n' +
+                     '}';
+        var output = '.phone_title {\n' +
+                     '    width: 500px\n' +
+                     '}\n' +
+                     '@media (max-width: 500px) {\n' +
+                     '    .phone_title {\n' +
+                     '        width: auto\n' +
+                     '    }\n' +
+                     '}\n' +
+                     'body.is_dark .phone_title {\n' +
+                     '    color: white;\n' +
+                     '}\n' +
+                     '.phone img {\n' +
+                     '    display: block;\n' +
+                     '}';
+        check(input, output);
     });
 
 });
