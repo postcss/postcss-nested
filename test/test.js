ead004c6db306b538fbde4dc0707407ceb78f0d6
Andrey Sitnik <andrey@sitnik.ru>
2015-05-09 03:01:16 +0300

    Remove unnecessary test

--- test/test.js
+++ test/test.js
@@ -102,9 +102,4 @@ describe('postcss-nested', function () {
         check(input, output);
     });
 
-    it('works as function', function() {
-        var process = postcss(nested());
-        expect( process.process('a { b {} }').css ).to.equal('a b {}');
-    });
-
 });
