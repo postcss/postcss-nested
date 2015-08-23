9a2d79809aa77050bd4399946b6f3a40d6ed1c77
Andrey Sitnik <andrey@sitnik.ru>
2015-08-23 23:30:00 +0300

    Latest gulpfile.js from boilerplate

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
 
+var files = ['index.js', 'test/*.js', 'gulpfile.js'];
+
 gulp.task('lint', function () {
     var eslint = require('gulp-eslint');
-    return gulp.src(['index.js', 'test/*.js', 'gulpfile.js'])
+    return gulp.src(files)
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
@@ -10,7 +12,12 @@ gulp.task('lint', function () {
 
 gulp.task('test', function () {
     var mocha = require('gulp-mocha');
-    return gulp.src('test/*.js', { read: false }).pipe(mocha());
+    return gulp.src('test/*.js', { read: false })
+        .pipe(mocha());
 });
 
 gulp.task('default', ['lint', 'test']);
+
+gulp.task('watch', function () {
+    gulp.watch(files, ['lint', 'test']);
+});
