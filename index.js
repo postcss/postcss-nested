5754da3ba12103f82805c0141ef8aefec6300cad
Andrey Sitnik <andrey@sitnik.ru>
2018-09-07 15:37:41 +0300

    Update ESLint config

--- index.js
+++ index.js
@@ -1,124 +1,124 @@
-var postcss = require('postcss');
-var parser = require('postcss-selector-parser');
+var postcss = require('postcss')
+var parser = require('postcss-selector-parser')
 
-function parse(str) {
-    var nodes;
-    var saver = parser(function (parsed) {
-        nodes = parsed;
-    });
-    saver.processSync(str);
-    return nodes.at(0);
+function parse (str) {
+  var nodes
+  var saver = parser(function (parsed) {
+    nodes = parsed
+  })
+  saver.processSync(str)
+  return nodes.at(0)
 }
 
-function replace(nodes, parent) {
-    var replaced = false;
-    nodes.each(function (i) {
-        if (i.type === 'nesting') {
-            i.replaceWith(parent.clone());
-            replaced = true;
-        } else if (i.nodes) {
-            if (replace(i, parent)) {
-                replaced = true;
-            }
-        }
-    });
-    return replaced;
-}
-
-function selectors(parent, child) {
-    var result = [];
-    parent.selectors.forEach(function (i) {
-        var parentNode = parse(i);
-
-        child.selectors.forEach(function (j) {
-            var node = parse(j);
-            var replaced = replace(node, parentNode);
-            if (!replaced) {
-                node.prepend(parser.combinator({ value: ' ' }));
-                node.prepend(parentNode.clone());
-            }
-            result.push(node.toString());
-        });
-    });
-    return result;
-}
-
-function pickComment(comment, after) {
-    if ( comment && comment.type === 'comment' ) {
-        after.after(comment);
-        return comment;
-    } else {
-        return after;
+function replace (nodes, parent) {
+  var replaced = false
+  nodes.each(function (i) {
+    if (i.type === 'nesting') {
+      i.replaceWith(parent.clone())
+      replaced = true
+    } else if (i.nodes) {
+      if (replace(i, parent)) {
+        replaced = true
+      }
     }
+  })
+  return replaced
 }
 
-function atruleChilds(rule, atrule) {
-    var children = [];
-    atrule.each(function (child) {
-        if ( child.type === 'comment' ) {
-            children.push( child );
-        } if ( child.type === 'decl' ) {
-            children.push( child );
-        } else if ( child.type === 'rule' ) {
-            child.selectors = selectors(rule, child);
-        } else if ( child.type === 'atrule' ) {
-            atruleChilds(rule, child);
-        }
-    });
-    if ( atrule.name === 'font-face' ) return;
+function selectors (parent, child) {
+  var result = []
+  parent.selectors.forEach(function (i) {
+    var parentNode = parse(i)
 
-    if ( children.length ) {
-        var clone = rule.clone({ nodes: [] });
-        for ( var i = 0; i < children.length; i++ ) {
-            clone.append(children[i]);
-        }
-        atrule.prepend(clone);
-    }
+    child.selectors.forEach(function (j) {
+      var node = parse(j)
+      var replaced = replace(node, parentNode)
+      if (!replaced) {
+        node.prepend(parser.combinator({ value: ' ' }))
+        node.prepend(parentNode.clone())
+      }
+      result.push(node.toString())
+    })
+  })
+  return result
 }
 
-function processRule(rule, bubble, preserveEmpty) {
-    var unwrapped = false;
-    var after     = rule;
-    rule.each(function (child) {
-        if ( child.type === 'rule' ) {
-            unwrapped = true;
-            child.selectors = selectors(rule, child);
-            after = pickComment(child.prev(), after);
-            after.after(child);
-            after = child;
-        } else if ( child.type === 'atrule' ) {
-            if ( bubble.indexOf(child.name) !== -1 ) {
-                unwrapped = true;
-                atruleChilds(rule, child);
-                after = pickComment(child.prev(), after);
-                after.after(child);
-                after = child;
-            }
-        }
-    });
-    if ( unwrapped && preserveEmpty !== true ) {
-        rule.raws.semicolon = true;
-        if ( rule.nodes.length === 0 ) rule.remove();
+function pickComment (comment, after) {
+  if (comment && comment.type === 'comment') {
+    after.after(comment)
+    return comment
+  } else {
+    return after
+  }
+}
+
+function atruleChilds (rule, atrule) {
+  var children = []
+  atrule.each(function (child) {
+    if (child.type === 'comment') {
+      children.push(child)
+    } if (child.type === 'decl') {
+      children.push(child)
+    } else if (child.type === 'rule') {
+      child.selectors = selectors(rule, child)
+    } else if (child.type === 'atrule') {
+      atruleChilds(rule, child)
     }
+  })
+  if (atrule.name === 'font-face') return
+
+  if (children.length) {
+    var clone = rule.clone({ nodes: [] })
+    for (var i = 0; i < children.length; i++) {
+      clone.append(children[i])
+    }
+    atrule.prepend(clone)
+  }
+}
+
+function processRule (rule, bubble, preserveEmpty) {
+  var unwrapped = false
+  var after = rule
+  rule.each(function (child) {
+    if (child.type === 'rule') {
+      unwrapped = true
+      child.selectors = selectors(rule, child)
+      after = pickComment(child.prev(), after)
+      after.after(child)
+      after = child
+    } else if (child.type === 'atrule') {
+      if (bubble.indexOf(child.name) !== -1) {
+        unwrapped = true
+        atruleChilds(rule, child)
+        after = pickComment(child.prev(), after)
+        after.after(child)
+        after = child
+      }
+    }
+  })
+  if (unwrapped && preserveEmpty !== true) {
+    rule.raws.semicolon = true
+    if (rule.nodes.length === 0) rule.remove()
+  }
 }
 
 module.exports = postcss.plugin('postcss-nested', function (opts) {
-    var bubble = ['media', 'supports', 'document', 'font-face'];
-    if ( opts && opts.bubble ) {
-        bubble = bubble.concat(opts.bubble.map(function (i) {
-            return i.replace(/^@/, '');
-        }));
-    }
-    var preserveEmpty = opts ? opts.preserveEmpty : false;
+  var bubble = ['media', 'supports', 'document', 'font-face']
+  if (opts && opts.bubble) {
+    bubble = bubble.concat(opts.bubble.map(function (i) {
+      return i.replace(/^@/, '')
+    }))
+  }
+  var preserveEmpty = opts ? opts.preserveEmpty : false
 
-    var process = function (node) {
-        node.each(function (child) {
-            if ( child.type === 'rule' ) {
-                processRule(child, bubble, preserveEmpty);
-            } else if ( child.type === 'atrule' ) {
-                process(child);
-            }
-        });
-    };
-    return process;
-});
+  var process = function (node) {
+    node.each(function (child) {
+      if (child.type === 'rule') {
+        processRule(child, bubble, preserveEmpty)
+      } else if (child.type === 'atrule') {
+        process(child)
+      }
+    })
+  }
+  return process
+})
--- index.test.js
+++ index.test.js
@@ -1,155 +1,155 @@
-var postcss = require('postcss');
+var postcss = require('postcss')
 
-var plugin = require('./');
+var plugin = require('./')
 
-function run(input, output, opts) {
-    return postcss([ plugin(opts) ]).process(input, { from: undefined })
-        .then(result => {
-            expect(result.css).toEqual(output);
-            expect(result.warnings().length).toBe(0);
-        });
+function run (input, output, opts) {
+  return postcss([plugin(opts)]).process(input, { from: undefined })
+    .then(function (result) {
+      expect(result.css).toEqual(output)
+      expect(result.warnings()).toHaveLength(0)
+    })
 }
 
-it('unwraps rule inside rule', () => {
-    return run(
-        'a { a: 1 } a { a: 1; b { b: 2; c { c: 3 } } }',
-        'a { a: 1 } a { a: 1; } a b { b: 2; } a b c { c: 3 }');
-});
+it('unwraps rule inside rule', function () {
+  return run(
+    'a { a: 1 } a { a: 1; b { b: 2; c { c: 3 } } }',
+    'a { a: 1 } a { a: 1; } a b { b: 2; } a b c { c: 3 }')
+})
 
-it('cleans rules after unwrap', () => {
-    return run(
-        'a { b .one {} b .two {} }',
-        'a b .one {} a b .two {}');
-});
+it('cleans rules after unwrap', function () {
+  return run(
+    'a { b .one {} b .two {} }',
+    'a b .one {} a b .two {}')
+})
 
-it('preserve empty rules if preserveEmpty is set to true', () => {
-    return run(
-        'a { b .one {} b .two {} }',
-        'a { } a b .one {} a b .two {}', { preserveEmpty: true });
-});
+it('preserve empty rules if preserveEmpty is set to true', function () {
+  return run(
+    'a { b .one {} b .two {} }',
+    'a { } a b .one {} a b .two {}', { preserveEmpty: true })
+})
 
-it('replaces ampersand', () => {
-    return run(
-        'a { body &:hover b {} }',
-        'body a:hover b {}');
-});
+it('replaces ampersand', function () {
+  return run(
+    'a { body &:hover b {} }',
+    'body a:hover b {}')
+})
 
-it('replaces ampersands', () => {
-    return run(
-        'a { &:hover, &:active {} }',
-        'a:hover, a:active {}');
-});
+it('replaces ampersands', function () {
+  return run(
+    'a { &:hover, &:active {} }',
+    'a:hover, a:active {}')
+})
 
-it('replaces ampersand in string', () => {
-    return run(
-        '.block { &_elem {} }',
-        '.block_elem {}');
-});
+it('replaces ampersand in string', function () {
+  return run(
+    '.block { &_elem {} }',
+    '.block_elem {}')
+})
 
-it('unwrap rules inside at-rules', () => {
-    return run(
-        '@media (max-width: 500px) { a { b {} } }',
-        '@media (max-width: 500px) { a b {} }');
-});
+it('unwrap rules inside at-rules', function () {
+  return run(
+    '@media (max-width: 500px) { a { b {} } }',
+    '@media (max-width: 500px) { a b {} }')
+})
 
-it('unwraps at-rule', () => {
-    return run(
-        'a { b { @media screen { width: auto } } }',
-        '@media screen {a b { width: auto } }');
-});
+it('unwraps at-rule', function () {
+  return run(
+    'a { b { @media screen { width: auto } } }',
+    '@media screen {a b { width: auto } }')
+})
 
-it('unwraps at-rule with rules', () => {
-    return run(
-        'a { @media screen { b { color: black } } }',
-        '@media screen { a b { color: black } }');
-});
+it('unwraps at-rule with rules', function () {
+  return run(
+    'a { @media screen { b { color: black } } }',
+    '@media screen { a b { color: black } }')
+})
 
-it('unwraps font-face to top level css', () => {
-    return run(
-        '.a { @font-face { font-family:font; src:url() format("woff"); } }',
-        '@font-face { font-family:font; src:url() format("woff"); }'
-    );
-});
+it('unwraps font-face to top level css', function () {
+  return run(
+    '.a { @font-face { font-family:font; src:url() format("woff"); } }',
+    '@font-face { font-family:font; src:url() format("woff"); }'
+  )
+})
 
-it('unwraps multiple fonts to top level css', () => {
-    return run(
-        '.a { @font-face { font-family:f1; } @font-face { font-family:f2; }}',
-        '@font-face { font-family:f1; } @font-face { font-family:f2; }'
-    );
-});
+it('unwraps multiple fonts to top level css', function () {
+  return run(
+    '.a { @font-face { font-family:f1; } @font-face { font-family:f2; }}',
+    '@font-face { font-family:f1; } @font-face { font-family:f2; }'
+  )
+})
 
-it('unwraps at-rules', () => {
-    return run(
-        'a { a: 1 } a { @media screen { @supports (a: 1) { a: 1 } } }',
-        'a { a: 1 } @media screen { @supports (a: 1) { a { a: 1 } } }');
-});
+it('unwraps at-rules', function () {
+  return run(
+    'a { a: 1 } a { @media screen { @supports (a: 1) { a: 1 } } }',
+    'a { a: 1 } @media screen { @supports (a: 1) { a { a: 1 } } }')
+})
 
-it('do not move custom at-rules', () => {
-    return run(
-        '.one { @mixin test; } .two { @phone { color: black } }',
-        '.one { @mixin test; } @phone { .two { color: black } }',
-        { bubble: ['phone'] });
-});
+it('do not move custom at-rules', function () {
+  return run(
+    '.one { @mixin test; } .two { @phone { color: black } }',
+    '.one { @mixin test; } @phone { .two { color: black } }',
+    { bubble: ['phone'] })
+})
 
-it('supports bubble option with at-name', () => {
-    return run(
-        'a { @phone { color: black } }',
-        '@phone {a { color: black } }',
-        { bubble: ['@phone'] });
-});
+it('supports bubble option with at-name', function () {
+  return run(
+    'a { @phone { color: black } }',
+    '@phone {a { color: black } }',
+    { bubble: ['@phone'] })
+})
 
-it('processes comma', () => {
-    return run(
-        '.one, .two { a {} }',
-        '.one a, .two a {}');
-});
+it('processes comma', function () {
+  return run(
+    '.one, .two { a {} }',
+    '.one a, .two a {}')
+})
 
-it('processes comma with ampersand', () => {
-    return run(
-        '.one, .two { &:hover {} }',
-        '.one:hover, .two:hover {}');
-});
+it('processes comma with ampersand', function () {
+  return run(
+    '.one, .two { &:hover {} }',
+    '.one:hover, .two:hover {}')
+})
 
-it('processes comma inside', () => {
-    return run(
-        'a, b { .one, .two {} }',
-        'a .one, a .two, b .one, b .two {}');
-});
+it('processes comma inside', function () {
+  return run(
+    'a, b { .one, .two {} }',
+    'a .one, a .two, b .one, b .two {}')
+})
 
-it('moves comment with rule', () => {
-    return run(
-        'a { /*B*/ b {} }',
-        '/*B*/ a b {}');
-});
+it('moves comment with rule', function () {
+  return run(
+    'a { /*B*/ b {} }',
+    '/*B*/ a b {}')
+})
 
-it('moves comment with at-rule', () => {
-    return run(
-        'a { /*B*/ @media { one: 1 } }',
-        '/*B*/ @media {a { one: 1 } }');
-});
+it('moves comment with at-rule', function () {
+  return run(
+    'a { /*B*/ @media { one: 1 } }',
+    '/*B*/ @media {a { one: 1 } }')
+})
 
-it('moves comment with declaration', () => {
-    return run(
-        'a { @media { /*B*/ one: 1 } }',
-        '@media {a { /*B*/ one: 1 } }');
-});
+it('moves comment with declaration', function () {
+  return run(
+    'a { @media { /*B*/ one: 1 } }',
+    '@media {a { /*B*/ one: 1 } }')
+})
 
-it('saves order of rules', () => {
-    return run(
-        '.one { & .two {} & .tree {} }',
-        '.one .two {} .one .tree {}');
-});
+it('saves order of rules', function () {
+  return run(
+    '.one { & .two {} & .tree {} }',
+    '.one .two {} .one .tree {}')
+})
 
-it('does not replace ampersand inside string', () => {
-    return run(
-        'div { &[data-category="sound & vision"] {} }',
-        'div[data-category="sound & vision"] {}');
-});
+it('does not replace ampersand inside string', function () {
+  return run(
+    'div { &[data-category="sound & vision"] {} }',
+    'div[data-category="sound & vision"] {}')
+})
 
-it('replaces ampersand in adjacent sibling selector', () => {
-    return run('div { & + & {} }', 'div + div {}');
-});
+it('replaces ampersand in adjacent sibling selector', function () {
+  return run('div { & + & {} }', 'div + div {}')
+})
 
-it('replaces ampersands in not selector', () => {
-    return run('.a { &:not(&.no) {} }', '.a:not(.a.no) {}');
-});
+it('replaces ampersands in not selector', function () {
+  return run('.a { &:not(&.no) {} }', '.a:not(.a.no) {}')
+})
